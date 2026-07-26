8b67d779c76164d92e225ee281ef4ac279a6c34c
ahmedmt <ahmedmt411@gmail.com>
2026-07-26 20:52:42 +0300

    feat: add shadcn components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
@@ -8,7 +7,6 @@ function Home() {
     <div className="p-8">
       <div>
         <p>tanstack start</p>
-        <Button>click me</Button>
       </div>
     </div>
   )
